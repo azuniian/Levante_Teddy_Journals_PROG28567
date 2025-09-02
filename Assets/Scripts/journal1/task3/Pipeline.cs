23d9897b325fd53a550cefdf36ac385c0fca3728
Teddy Levante <levantea@OA-S145-06>
2025-09-02 14:09:33 -0400

    updated array to arraylist by adding the generic collections use line code will update the mouse position and index on mouse down; need to figure out how to fix it so that it will do the same while being held down...may need to use loops

--- Assets/Scripts/journal1/task3/Pipeline.cs
+++ Assets/Scripts/journal1/task3/Pipeline.cs
@@ -1,35 +1,83 @@
 using System.Collections;
+using System.Collections.Generic; //referenced Unity API scripting (find link) to figure out how to get normal Lists to work with Unity 6000
 using Unity.Android.Types;
 using Unity.VisualScripting;
 using UnityEngine;
 
 public class Pipeline : MonoBehaviour
 {
-    public Vector2 mousePosition;
-    public Transform mousePos;
-    public Vector2[] mousePositions;
-    Vector2 origin = new Vector2(0, 0); //very start
+    //public Vector2 mousePosition;
+    //public Transform mousePos;
+    public List<Vector2> mousePositions = new List<Vector2>();
 
     public int currentPos = 1; //array int
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        mousePositions = new Vector2[];
-        mousePositions[currentPos] = origin;
-        currentPos++;
+       
     }
 
     // Update is called once per frame
     void Update()
     {
-        //variables 
-        
-        Vector2 start = new Vector2(); //previous end position for line drawing
-        Vector2 end = new Vector2(); //current end position for line drawing (mousePos)
-        //Vector2 newLine = new Vector2(); 
+        Vector2 mousePos = Input.mousePosition;
+        if (Input.GetMouseButtonDown(0))
+        {
+            mousePositions.Add(mousePos);
+            Debug.Log(mousePositions[currentPos]);
+            currentPos++;
 
-        mousePosition = Input.mousePosition; //thank you Kano for helping me remember how to get a mouse position 
+            if (Input.GetMouseButton(0))
+            {
+                //mousePositions.Add(mousePos);
+                //Debug.DrawLine(mousePositions[currentPos - 1], mousePositions[currentPos]);
+                //currentPos++;
+            }
+        }
+
+        
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+    Debug.Log(currentPos);
+
+        //variables 
+
+        Vector2 start = new Vector2(); //previous end position for line drawing
+    Vector2 end = new Vector2(); //current end position for line drawing (mousePos)
+    Vector2 origin = new Vector2(0, 0); //very start
+
+    mousePosition = Input.mousePosition; //thank you Kano for helping me remember how to get a mouse position 
         
         //get mouse position
         if(Input.GetMouseButtonDown(0) == true)
@@ -39,16 +87,17 @@ public class Pipeline : MonoBehaviour
 
         }
 
-        if (Input.GetMouseButton(0) == true)
-        {
-            currentPos++;
+if (Input.GetMouseButton(0) == true)
+{
 
-            end = mousePos.position;
-            mousePositions[currentPos] = end;
-            start = mousePositions[currentPos - 1];
+    //end = mousePos.position;
+    //mousePositions[currentPos] = end;
+    //start = mousePositions[currentPos - 1];
 
-            Debug.Log(currentPos);
-        }
+    //Debug.Log(currentPos);
+
+    //currentPos++;
+}*/
+
         
-    }
 }
