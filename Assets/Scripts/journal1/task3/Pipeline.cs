9f3f0ce3b36decd6e22492523c3bdcc2166f3e5c
Teddy Levante <levantea@OA-S145-06>
2025-09-04 13:40:48 -0400

    worked on task 3 again - figured out how to get the array to properly populate, and a line to draw; just need to figure out how to get the new point into the list without getting a indexOutOfBounds error
    going to try the other two tasks, then try again later!

--- Assets/Scripts/journal1/task3/Pipeline.cs
+++ Assets/Scripts/journal1/task3/Pipeline.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic; //referenced Unity API scripting (find link) to figure out how to get normal Lists to work with Unity 6000
+using System.Net.NetworkInformation;
 using Unity.Android.Types;
 using Unity.VisualScripting;
 using UnityEngine;
@@ -8,96 +9,43 @@ public class Pipeline : MonoBehaviour
 {
     //public Vector2 mousePosition;
     //public Transform mousePos;
+    
     public List<Vector2> mousePositions = new List<Vector2>();
-
-    public int currentPos = 1; //array int
+    public int lastPos = 0; //array int
+    float time = 0;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-       
     }
 
     // Update is called once per frame
     void Update()
     {
         Vector2 mousePos = Input.mousePosition;
-        if (Input.GetMouseButtonDown(0))
+        Vector2 origin = new Vector2(0, 0);
+
+        if (Input.GetMouseButton(0))
         {
             mousePositions.Add(mousePos);
-            Debug.Log(mousePositions[currentPos]);
-            currentPos++;
+            Debug.Log(mousePositions[lastPos]);
+            
 
-            if (Input.GetMouseButton(0))
+            if(time >= 0.1)
             {
-                //mousePositions.Add(mousePos);
-                //Debug.DrawLine(mousePositions[currentPos - 1], mousePositions[currentPos]);
-                //currentPos++;
+                mousePositions.Add(mousePos);
+                Debug.DrawLine(mousePositions[lastPos], origin); //currently drawing a line between the current point and itself...need timer to add a new point after 0.1 seconds so i can then draw line between
+                lastPos++;
+                time = 0;
             }
+            
+            
         }
 
-        
+
+        time += Time.deltaTime;
 
     }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/*
-    Debug.Log(currentPos);
-
-        //variables 
-
-        Vector2 start = new Vector2(); //previous end position for line drawing
-    Vector2 end = new Vector2(); //current end position for line drawing (mousePos)
-    Vector2 origin = new Vector2(0, 0); //very start
-
-    mousePosition = Input.mousePosition; //thank you Kano for helping me remember how to get a mouse position 
-        
-        //get mouse position
-        if(Input.GetMouseButtonDown(0) == true)
-        {
-            mousePos.position = mousePosition;
-            Debug.Log(mousePos.position);
-
-        }
-
-if (Input.GetMouseButton(0) == true)
-{
-
-    //end = mousePos.position;
-    //mousePositions[currentPos] = end;
-    //start = mousePositions[currentPos - 1];
-
-    //Debug.Log(currentPos);
-
-    //currentPos++;
-}*/
 
         
 }
