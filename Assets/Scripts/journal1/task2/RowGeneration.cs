be3d00da1434bd083dd41360aa6d188da729c129
laptebby <azuniian895@gmail.com>
2025-09-08 18:01:17 -0400

    task 2: somehow, the commit and push I made didn't count, so I'll be re-committing and pushing the same information but again second half of time working on this task square spawner spawns based off of input in input field checks for valid entries

--- Assets/Scripts/journal1/task2/RowGeneration.cs
+++ Assets/Scripts/journal1/task2/RowGeneration.cs
@@ -20,6 +20,31 @@ public class RowGeneration : MonoBehaviour
 
     public void TextChecker()
     {
-        Debug.Log(inputField.text);
+        //Debug.Log(inputField.text);
+        if (int.TryParse(inputField.text, out int value) && value >= 0)
+        {
+            for (int i = 0; i < value, i++) {
+                //create a new point that changes position for each iteration of the for loop
+                Vector2 newPoint = new Vector2(i, 0);
+
+                //re-used code from square spawner
+                Vector2 topLeft = new Vector2(newPoint.x, newPoint.y);
+                Vector2 topRight = new Vector2(newPoint.x + 1, newPoint.y);
+                Vector2 botLeft = new Vector2(newPoint.x, newPoint.y - 1);
+                Vector2 botRight = new Vector2(newPoint.x + 1, newPoint.y - 1);
+
+                //re-used spawn square code from square spawner
+                Debug.DrawLine(topLeft, botLeft, Color.white, 5);
+                Debug.DrawLine(topLeft, topRight, Color.white, 5);
+                Debug.DrawLine(topRight, botRight, Color.white, 5);
+                Debug.DrawLine(botRight, botLeft, Color.white, 5);
+            }
+            //Debug.Log(value);
+        }
+
+        else
+        {
+            Debug.Log("not a valid number/entry");
+        }
     }
 }
