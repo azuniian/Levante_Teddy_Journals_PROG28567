b14f5da5036c9867dc444ea1278c04b191e6ff1c
laptebby <azuniian895@gmail.com>
2025-09-08 15:23:34 -0400

    task 1: updated the square spawner to check the current scoll wheel value as the square side length put the drawing square code found in the click if statement into update with a new colour (semi-transparent alpha) so that the square will draw at mouse position every frame

--- Assets/Scripts/journal1/task1/SquareSpawner.cs
+++ Assets/Scripts/journal1/task1/SquareSpawner.cs
@@ -8,7 +8,9 @@ public class SquareSpawner : MonoBehaviour
     Vector2 squareLeftBottom = new Vector2(); //bottom left corner position
     Vector2 squareRightBottom = new Vector2(); //bottom right corner position
 
-    float sizeIncrease; //what scroll wheel will increase
+    float sideSize = 1; //what scroll wheel will increase
+
+    Color semiTrans = new Color(1, 1, 1, 0.5f);
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -22,31 +24,46 @@ public class SquareSpawner : MonoBehaviour
         Vector2 mousePos = new Vector2(Input.mousePosition.x, Input.mousePosition.y);
         Vector2 squarePos = Camera.main.ScreenToWorldPoint(mousePos);
 
+        
+        //get the changing value of side size based on scroll wheel
+        sideSize += Input.mouseScrollDelta.y;
+        Debug.Log(sideSize);
+       
+
+        //get new coordinates for the top left corner
+        squareLeftTop.x = squarePos.x - sideSize;
+        squareLeftTop.y = squarePos.y + sideSize;
+
+        //get new coordinates for the top right corner
+        squareRightTop.x = squarePos.x + sideSize;
+        squareRightTop.y = squarePos.y + sideSize;
+
+        //get new coordinates for the bottom left corner
+        squareLeftBottom.x = squarePos.x - sideSize;
+        squareLeftBottom.y = squarePos.y - sideSize;
+
+        //get new coordinates for the top left corner
+        squareRightBottom.x = squarePos.x + sideSize;
+        squareRightBottom.y = squarePos.y - sideSize;
+
+
+
         if (Input.GetMouseButtonDown(0))
         {
-            //get new coordinates for the top left corner
-            squareLeftTop.x = squarePos.x - 2;
-            squareLeftTop.y = squarePos.y + 2;
-
-            //get new coordinates for the top right corner
-            squareRightTop.x = squarePos.x + 2;
-            squareRightTop.y = squarePos.y + 2;
-
-            //get new coordinates for the bottom left corner
-            squareLeftBottom.x = squarePos.x - 2;
-            squareLeftBottom.y = squarePos.y - 2;
-
-            //get new coordinates for the top left corner
-            squareRightBottom.x = squarePos.x + 2;
-            squareRightBottom.y = squarePos.y - 2;
-
-
-            //draw square
+            //draw square on click
             Debug.DrawLine(squareLeftTop, squareLeftBottom, Color.white, Mathf.Infinity);
             Debug.DrawLine(squareLeftTop, squareRightTop, Color.white, Mathf.Infinity);
             Debug.DrawLine(squareRightTop, squareRightBottom, Color.white, Mathf.Infinity);
             Debug.DrawLine(squareRightBottom, squareLeftBottom, Color.white, Mathf.Infinity);
         }
-        
+
+
+        //draw square at mouse position
+        Debug.DrawLine(squareLeftTop, squareLeftBottom, semiTrans);
+        Debug.DrawLine(squareLeftTop, squareRightTop, semiTrans);
+        Debug.DrawLine(squareRightTop, squareRightBottom, semiTrans);
+        Debug.DrawLine(squareRightBottom, squareLeftBottom, semiTrans);
+
+
     }
 }
