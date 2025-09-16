c309bfd13d93b16e0c39e7a801c721e16f38d9df
Teddy Levante <levantea@OA-S141-08>
2025-09-16 12:56:51 -0400

    journal/assignment 3: task 4 (constellation) created the method and method call, and figured out how to get all lines to draw at once added comments detailing what i think the next steps are

--- Assets/Scripts/Controllers/Stars.cs
+++ Assets/Scripts/Controllers/Stars.cs
@@ -5,10 +5,42 @@ using UnityEngine;
 public class Stars : MonoBehaviour
 {
     public List<Transform> starTransforms;
-    public float drawingTime;
+    public float drawingTime = 5;
+
+    public float acceleration;
+    public float drawSpeed = 3;
+
+    private void Start()
+    {
+        acceleration = drawSpeed / drawingTime;
+    }
 
     // Update is called once per frame
     void Update()
     {
+        if (Input.GetMouseButtonDown(0))
+        {
+            DrawConstellation();
+        }
+    }
+
+    public void DrawConstellation()
+    {
+
+        for(int i = 0; i < starTransforms.Count; i++)
+        {
+            float distance = Vector3.Distance(starTransforms[i].position, starTransforms[i + 1].position);
+            //create direction vector between the two stars
+            //normalize direction vector
+            //create velocity vector and update it with acceleration logic
+            //multiply by the normalized direction vector by the velocity until the magnitude of the direction vector is >= the distance between the stars
+            //draw the line with each different velocity value (each time the acceleration is added to the velocity
+            
+        }
+
+        //Vector3 currentStar = starTransforms[i].position;
+        //Vector3 nextStar = starTransforms[i+1].position;
+
+        //Debug.DrawLine(currentStar, nextStar, Color.white, drawingTime);
     }
 }
