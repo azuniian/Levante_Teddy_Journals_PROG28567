9e42803884fd2cbd87cf56c1502cb0696642fa59
laptebby <azuniian895@gmail.com>
2025-09-22 17:48:31 -0400

    journal 3 task 4: constellation i have the logic that i think will work set up, but i'm getting an argument out of bounds error...need to check what could be causing it once i get my computer charged a bit more.

--- Assets/Scripts/Controllers/Stars.cs
+++ Assets/Scripts/Controllers/Stars.cs
@@ -30,10 +30,20 @@ public class Stars : MonoBehaviour
         for(int i = 0; i < starTransforms.Count; i++)
         {
             float distance = Vector3.Distance(starTransforms[i].position, starTransforms[i + 1].position);
-            //create direction vector between the two stars
-            //normalize direction vector
+            
+            //create direction vector between the two stars and then normalize it to get a magnitude of 1
+            Vector3 direction = (starTransforms[i].position - starTransforms[i+1].position).normalized;
+
             //create velocity vector and update it with acceleration logic
+            Vector3 velocity = direction * acceleration * Time.deltaTime;
+
             //multiply by the normalized direction vector by the velocity until the magnitude of the direction vector is >= the distance between the stars
+            if(direction.magnitude < distance)
+            {
+                float distCovered = Time.deltaTime * drawSpeed;
+                Vector3 currentPos = Vector3.Lerp(starTransforms[i].position, starTransforms[i + 1].position, distCovered / distance);
+                Debug.DrawLine(starTransforms[i].position, currentPos);
+            }
             //draw the line with each different velocity value (each time the acceleration is added to the velocity
             
         }
