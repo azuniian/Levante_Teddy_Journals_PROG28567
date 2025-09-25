df4f86faf6d60aac21ea3f5a268c40059c981d36
laptebby <azuniian895@gmail.com>
2025-09-24 22:04:50 -0400

    journal 3 task 2: enemy implementation i was able to implement the directional and acceleration-based movement; however, something is up with my conditional statements or something since the enemy ship won't stop spinning around the player ship?

--- Assets/Scripts/Controllers/Enemy.cs
+++ Assets/Scripts/Controllers/Enemy.cs
@@ -1,10 +1,61 @@
 ﻿using UnityEngine;
 using System.Collections;
+using Unity.VisualScripting;
+using System.Collections.Generic;
 
 public class Enemy : MonoBehaviour
 {
-    private void Update()
+    public Vector3 velocity;
+
+    private float acceleration;
+
+    public float accelerationTime = 3;
+    public float decelerationTime = 1;
+    public float maxSpeed = 5f;
+
+    public Transform playerPos;
+
+    public float arrivalDist = 0.5f;
+
+    public List<Vector3> warpPositions = new List<Vector3>();
+    
+
+    void Start()
     {
+        acceleration = (maxSpeed / accelerationTime);
+
+        for (float i = 0;  i < warpPositions.Count; i += i*Time.deltaTime)
+        {
+            int j = (int)i;
+            transform.position = warpPositions[j];
+        }
+    }
+
+    void Update()
+    {
+        
+
+        Vector3 direction = Vector3.zero;
+        Vector3 playerVector = new Vector3(playerPos.position.x, playerPos.position.y, 0);
+        direction = playerVector - transform.position;
+        direction = direction.normalized;
+
+        //Debug.DrawLine(Vector3.zero, direction, Color.white);
+
+        if(Vector3.Distance(transform.position, playerPos.position) >= arrivalDist)
+        {
+            velocity += direction * acceleration * Time.deltaTime;
+            transform.position += velocity;
+        }
+
+        else
+        {
+            //Debug.Log("stopping");
+            velocity = Vector3.zero;
+        }
+        
+
+
     }
 
 }
