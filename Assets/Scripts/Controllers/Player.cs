c7fcfd0570c305434f1354771187c6862d738b44
laptebby <azuniian895@gmail.com>
2025-09-28 20:55:20 -0400

    implemented basic line drawing code based on trig angles into a coroutine currently, it draws the lines one at a time, which is good need to ensure that the coroutine's wait time is the same as the line duration to ensure that only the current line is being drawn need to figure out which line is which so that i can draw certain lines at the same time

--- Assets/Scripts/Controllers/Player.cs
+++ Assets/Scripts/Controllers/Player.cs
@@ -10,22 +10,80 @@ public class Player : MonoBehaviour
     public GameObject bombPrefab;
     public Transform bombsTransform;
 
-    public TMPro.TMP_InputField ratioInputField;
+    public TMPro.TMP_InputField pointsInputField;
 
+    public float shieldRadius;
+    public int numberOfPoints;
+    public float timeToWait;
 
+    public List<Vector3> unitCirclePoints = new List<Vector3>();
+
+    private IEnumerator drawCoroutine;
+    public bool coroutineStopped = false;
+
+    public void Start()
+    {
+        
+    }
 
     void Update()
     {
 
-        if (Input.GetMouseButtonDown(0))
+        if (Input.GetMouseButtonDown(0)) //check for mouse click
         {
-            if (float.TryParse(ratioInputField.text, out float value) && value >= 0 && value <= 1)
-            {
-                
-            }
-        }
+            Debug.Log("mouse clicked");
 
+            if (float.TryParse(pointsInputField.text, out float points) && points >= 0) //check to ensure that the value in the box is a valid number for the math
+            {
+                drawCoroutine = drawSections(points, timeToWait);
+                StartCoroutine(drawSections(points, timeToWait));
+            }
+
+        }
 
     }
 
+
+    private IEnumerator drawSections(float points, float time)
+    {
+        Debug.Log("running");
+        float angleInDegrees = 360 / points; //get the angle needed for drawing each line segment in degrees
+        int i = 1;
+
+        while (i <= points)
+        {
+            //unit circle values exercise
+            float unitCircleX = Mathf.Cos((angleInDegrees * i) * Mathf.Deg2Rad); //get the current unit circle x value in radians from the list
+            float unitCircleY = Mathf.Sin((angleInDegrees * i) * Mathf.Deg2Rad); //get the current unit circle y value in radians from the list
+
+            Vector3 pointOnUnitCircle = new Vector3(unitCircleX, unitCircleY, 0); //create a vector3 to hold the current x and y values from the list
+            pointOnUnitCircle = pointOnUnitCircle * shieldRadius; //multiply it by the circle radius to change the line length
+
+            unitCirclePoints.Add(pointOnUnitCircle);
+            i++;
+        }
+
+        int j = 0;
+        while (j < points)
+        {
+            if (j < points - 1)
+            {
+                //Debug.Log(i);
+                Debug.DrawLine(unitCirclePoints[j], unitCirclePoints[j + 1], Color.green, 5);
+            }
+
+            else if (j == points - 1)
+            {
+                Debug.DrawLine(unitCirclePoints[unitCirclePoints.Count - 1], unitCirclePoints[0], Color.green, 5);
+            }
+            j++;
+            yield return new WaitForSeconds(time);
+        }
+
+        //clear so that this can all be re-used
+        unitCirclePoints.Clear();
+
+        //yield return null;
+        StopCoroutine(nameof(drawSections));
+    }
 }
