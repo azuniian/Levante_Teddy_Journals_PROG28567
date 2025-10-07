22491feba26753dae2370c245d17b72391193b5d
laptebby <azuniian895@gmail.com>
2025-10-07 13:59:59 -0400

    week 6: updated code to ensure that keely's code works need to set angular speed HIGH for it to work properly

--- Assets/Scripts/week6/Turret.cs
+++ Assets/Scripts/week6/Turret.cs
@@ -14,47 +14,52 @@ public class Turret : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        //draw a line between object and target
         Debug.DrawLine(transform.position, targetTransform.position, Color.blue);
+
         Vector3 directionToTarget = targetTransform.position - transform.position;
 
-        //can't use the dot product of the object's position and transform.right since itll only return moving left if the object is on the left side of the screen
-        //instead, use the DIRECTION VECTOR between the target's transform and the object's position and transform.right
+
         float dotProduct = Vector3.Dot(transform.right, directionToTarget);
+        float currentAngle = Mathf.Rad2Deg * Mathf.Atan2(transform.up.y, transform.up.x);
+        float targetAngle = Mathf.Rad2Deg * Mathf.Atan2(directionToTarget.y, directionToTarget.x);
 
-        if(dotProduct > 0) //if positive, move to the right
+        float angleRemaining = Mathf.DeltaAngle(currentAngle, targetAngle);
+        float changeInAngle;
+
+        //If dot product is positive
+        //The object is on the right hand side and we should rotate to the right
+        if (dotProduct > 0)
         {
-            float currentAngle = (Mathf.Atan2(transform.up.y, transform.up.x)) * Mathf.Rad2Deg;
-            float targetAngle = (Mathf.Atan2(directionToTarget.y, directionToTarget.x)) * Mathf.Rad2Deg;
+            //Negative will rotate to the right
+            changeInAngle = -angularSpeed * Time.deltaTime;
 
-            float remainingAngle = Mathf.DeltaAngle(currentAngle, targetAngle);
-            float changeInAngle = -angularSpeed * Time.deltaTime;
-
-            if (changeInAngle > remainingAngle)
+            //If the change that we're about to do would overshoot
+            if (changeInAngle < angleRemaining)
             {
-                transform.Rotate(0f, 0f, remainingAngle);
+                //Snap to the target
+                transform.Rotate(0f, 0f, angleRemaining);
+            }
+            else //If it wouldn't overshoot
+            {
+                //Rotate as normal
+                transform.Rotate(0f, 0f, changeInAngle);
+            }
+
+
+        }
+        else
+        {
+            changeInAngle = angularSpeed * Time.deltaTime;
+            //Move to the left
+            if (changeInAngle > angleRemaining)
+            {
+                transform.Rotate(0f, 0f, angleRemaining);
             }
             else
             {
                 transform.Rotate(0f, 0f, changeInAngle);
             }
         }
-
-        else //if negative, move to the left
-        {
-            
-
-            //if ()
-            //{
-            //    transform.Rotate(0f, 0f, angularSpeed);
-            //}
-            //else
-            //{
-            //    transform.rotation = targetTransform.rotation;
-            //}
-            
-        }
-
 
 
         ////////////////////////////EXERCISE FOR ROTATING WITH ANGULAR SPEED\\\\\\\\\\\\\\\\\\\\\\\\
@@ -63,15 +68,15 @@ public class Turret : MonoBehaviour
         ////same here - draws a line from the object to its up direction
         //Debug.DrawLine(transform.position, transform.up, Color.green);
 
-            //MY ATTEMPT AT THE STOP ROTATION LOGIC
-            ////this part is where i'm struggling at the moment - i need to fix my conditional logic so that it will actually stop rotating once it gets to a specific angle
-            //float currentRotationAngle = Mathf.Atan2(transform.up.y, transform.up.x); //make an angle out of a vector
-            //float currentRotationAngleDegrees = currentRotationAngle * Mathf.Rad2Deg; //convert it to degrees
-            ////Debug.Log(currentRotationAngleDegrees);
+        //MY ATTEMPT AT THE STOP ROTATION LOGIC
+        ////this part is where i'm struggling at the moment - i need to fix my conditional logic so that it will actually stop rotating once it gets to a specific angle
+        //float currentRotationAngle = Mathf.Atan2(transform.up.y, transform.up.x); //make an angle out of a vector
+        //float currentRotationAngleDegrees = currentRotationAngle * Mathf.Rad2Deg; //convert it to degrees
+        ////Debug.Log(currentRotationAngleDegrees);
 
-            //if(currentRotationAngleDegrees > 81 && currentRotationAngleDegrees < 82)
-            //{
-            //    angularSpeed = 0;
-            //}
+        //if(currentRotationAngleDegrees > 81 && currentRotationAngleDegrees < 82)
+        //{
+        //    angularSpeed = 0;
+        //}
     }
 }
